7516d2925cea0e48d3be2748d3c04d6584c53cff
Aleksandr Ivakhnenko <125494676+aleksandr-ivakhnenko@users.noreply.github.com>
2023-04-19 20:02:24 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,26 +1,13 @@
 const BURGER_OPENED_CLASSNAME = 'menu-burger_open';
 const BURGER_BTN_OPENED_CLASSNAME = 'button-menu-burger_open';
-const BODY_FIXED_CLASSNAME = 'body_fixed';
+const BODY_FIXED_CLASSNAME2 = 'body_fixed';
 
-const bodyNode = document.querySelector('body');
+const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-menu-burger');
 const burgerBtnNode = document.querySelector('.js-button-menu-burger');
 const burgerContentNode = document.querySelector('.js-menu-burger__content');
 
-/* const burgerNavLinksNodesAll = document.querySelectorAll('.menu-burger__link');
-
-const burgerNavLinksNodes = [burgerNavLinksNodesAll[0], 
-                            burgerNavLinksNodesAll[1], 
-                            burgerNavLinksNodesAll[2], 
-                            burgerNavLinksNodesAll[3]
-                            burgerNavLinksNodesAll[4]];
-console.log(burgerNavLinksNodes)
-
-burgerNavLinksNodes[0].addEventListener('click', toggleBurger);
-burgerNavLinksNodes[1].addEventListener('click', toggleBurger);
-burgerNavLinksNodes[2].addEventListener('click', toggleBurger);
-burgerNavLinksNodes[3].addEventListener('click', toggleBurger);
-burgerNavLinksNodes[4].addEventListener('click', toggleBurger); */
+const burgerNavLinksNodesAll = document.querySelectorAll('.menu-burger__link');
 
 burgerBtnNode.addEventListener('click', toggleBurger);
 
@@ -40,6 +27,6 @@ burgerNode.addEventListener('click', (event) => {
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
-    bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
+    bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME2);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
 };
---
+++ js/index.js
@@ -0,0 +1,2 @@
+const BODY_FIXED_CLASSNAME = 'body_fixed';
+const bodyNode = document.querySelector('body');
