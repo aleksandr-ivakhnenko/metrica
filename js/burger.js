d32513be00f7ee36926659c71c27cfb8dcb870b7
Aleksandr Ivakhnenko <125494676+aleksandr-ivakhnenko@users.noreply.github.com>
2023-04-19 13:04:48 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,12 @@
-const BURGER_OPENED_CLASSNAME = 'burger_open';
-const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BURGER_OPENED_CLASSNAME = 'menu-burger_open';
+const BURGER_BTN_OPENED_CLASSNAME = 'button-menu-burger_open';
+const BURGER_CONTENT_OPENED_CLASSNAME = 'menu-burger__content_open';
 const BODY_FIXED_CLASSNAME = 'body_fixed';
 
 const bodyNode = document.querySelector('body');
-const burgerNode = document.querySelector('.js-burger');
-const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerContentNode = document.querySelector('.js-burger__content')
+const burgerNode = document.querySelector('.js-menu-burger');
+const burgerBtnNode = document.querySelector('.js-button-menu-burger');
+const burgerContentNode = document.querySelector('.js-menu-burger__content');
 
 burgerBtnNode.addEventListener('click', toggleBurger);
 
@@ -21,4 +22,5 @@ function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
     bodyNode.classList.toggle(BODY_FIXED_CLASSNAME);
     burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+    burgerContentNode.classList.toggle(BURGER_CONTENT_OPENED_CLASSNAME);
 }
